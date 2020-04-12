560f3ed93e74406b29e7c420a3770b4aa01eeab5
Lebzo96Dev <44217902+Lebou@users.noreply.github.com>
2020-04-12 23:41:28 +0000

    Add files via upload

---
+++ qcm_Lebou_Ndiaye_DIEYE/pageDeJeux.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+
+if (isset($_POST['deconnexion'])){
+	header('Location: deconnexion.php');
+}
+if (!isset($_SESSION['login'])){
+	header('Location: index.php');
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Page d'accueil</title>
+	<link rel="stylesheet" type="text/css" href="pageDeJeux.css">
+</head>
+<body>
+
+<div class="header1">
+		<img class="logohead" src="Images/logo-QuizzSA.png">
+		<div class="titrehead">
+			<strong> Le plaisir de jouer</strong>
+		</div>
+	</div>
+	<div class="general">
+		<form method="post" action="">
+		<div class="barreDeconnexion">
+			<div class="profile">
+				<div style="margin-left: 5%; border: 2px solid black;width: 7vw; height: 7vw; float: left;">
+					profil
+			</div>
+			</div>
+			<div class="titreDeconnect">
+				<p align="center">
+			<strong>BIENVENUE SUR LA PLATEFORME DE JEU DE QUIZZ <br/> JOUER ET TESTER VOTRE NIVEAU DE CULTURE GENERALE</strong>
+			</p>
+			</div>
+			<div class="buttonDeconnect">
+			<input class="deconnexion" type="submit" name="deconnexion" value="Deconnexion">
+			</div>
+		</div>
+		</form>
+	</div>
+</body>
+</html>
